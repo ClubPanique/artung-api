72ff75f181484d5767222a27f94488745f30f8ab
Steve <sagmana@gmail.com>
2020-02-20 21:32:07 +0100

    ajout getinfosartiste

--- assets/vue/components/forms/FormGroup.vue
+++ assets/vue/components/forms/FormGroup.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <div>
-      <label for="contenu">{{ text }}</label>
-    </div>
+    <label :for="contenu">{{ text }}</label>
     <input
-      id="contenu"
-      placeholder="Champ requis"
+      :id="contenu"
+      :value="infosArtistResults.email"
+      :placeholder="placeholder"
     >
   </div>
 </template>
   <!-- Types : text, password, email -->
+
 
 <script>
 export default {
@@ -22,7 +22,28 @@ export default {
     typeStatus: {
       type: String,
       required: true
-    } 
+    },
+    contenu: {
+      type: String,
+      required: true
+    },
+    placeholder: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      infosArtistResults: null
+    };
+  },
+  computed: {
+    urlArtist() {
+      return `${window.rootUrl}fans/1`;
+    }
+  },
+  created() {
+    this.getInfosArtist();
   },
   mounted() {
     this.typeChange();
@@ -35,6 +56,15 @@ export default {
         this.$el.querySelector("input").setAttribute("type", "password");
       } else {
         this.$el.querySelector("input").setAttribute("type", "text");
+      }
+    },
+    async getInfosArtist() {
+      try {
+        const response = await fetch(this.urlArtist);
+        const result = await response.json();
+        this.infosArtistResults = result;
+      } catch (err) {
+        console.log(err);
       }
     }
   }
