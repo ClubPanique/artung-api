b60771856036d6a351fc33499b932adaf5dd2bc6
Adrien PHILIPPOT <adrienphilippot@gmail.com>
2020-02-21 16:32:24 +0100

    create components AccountButton and LoginButton

--- assets/vue/components/buttons/LoginButton.vue
+++ assets/vue/components/buttons/LoginButton.vue
@@ -1,5 +1,11 @@
 <template>
-  <div />
+  <div>
+    <a href="/Login">
+      <div id="dot">
+          <img src="../../assets/Arrow.svg" alt="right arrow">
+      </div>
+    </a>
+  </div>
 </template>
 
 <script>
@@ -10,5 +16,15 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  #dot {
+    height: 36px;
+    width: 36px;
+    background-color: #131B23;
+    border-radius: 50%;
+    border: 2px solid #DB5C4D;
+  }
 
+  #dot>img{
+    margin-inline-start: 4px;
+  }
 </style>
