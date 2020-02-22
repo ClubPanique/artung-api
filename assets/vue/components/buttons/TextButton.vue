b7868aad2cc19f7d9c2f2bc203495673bece023b
Steve <sagmana@gmail.com>
2020-02-22 16:16:26 +0100

    modif secondary vers js pour manipuler le css

--- assets/vue/components/buttons/TextButton.vue
+++ assets/vue/components/buttons/TextButton.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
-    <button
-      variant="outline-primary"
-      class="btn btn-outline-primary"
-      :class="{ secondary: secondary }"
+    <button    
+      <button
+      type="button"
+      class="btn btn-primary"
     >
       {{ text }}
     </button>
   </div>
 </template>
+<!-- Le classList.add("btn-secondary") ajoutera btn-secondary APRES btn-primary, Ã©crasant le CSS -->
 
 <script>
 export default {
@@ -21,43 +22,43 @@ export default {
     secondary: Boolean,
     form: Boolean
   },
-  data() {
-    return {
-      testVariable: "test"
-    };
-  },
-  computed: {
-  },
-
-  watch: {
-  },
   mounted() {
     // this.isSecondary();
     this.checkForm();
+    this.addSecondaryClass();
   },
   methods: {
-    // isSecondary: function() {
-    //   if (this.secondary) {
-    //     this.$el.querySelector("button").style.color = "#131b23";
-    //     this.$el.querySelector("button").style.background = "#db5c3d";
-    //     this.$el.querySelector("button").style.borderColor = "#db5c3d";
-    //   } else {
-    //     this.$el.querySelector("button").style.color = "#db5c3d";
-    //     this.$el.querySelector("button").style.background = "#131b23";
-    //     this.$el.querySelector("button").style.borderColor = "#db5c3d";
-    //   }
-    // },
     checkForm: function() {
       if (this.form) {
         this.$el.querySelector("button").setAttribute("type", "submit");
       } else {
         this.$el.querySelector("button").setAttribute("type", "button");
       }
-    }
+    },
+    addSecondaryClass: function() {
+      if (this.secondary) {
+        this.$el.querySelector("button").classList.add("btn-secondary");        
+      } else {
+        this.$el.querySelector("button").classList.remove("btn-secondary");
+        // alert("removed !")
+      }
+    },
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
+.btn-primary {
+  color: #db5c3d;
+  background: #131b23;
+  border-color: #db5c3d;
+}
+
+.btn-secondary {
+  color: #131b23;
+  background: #db5c3d;
+  border-color: #db5c3d;
+}
 </style>
