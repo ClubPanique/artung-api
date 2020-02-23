9d076fefbcbbbcc42cac88debdb07c07a6cb538d
Steve <sagmana@gmail.com>
2020-02-23 17:39:41 +0100

    ajout font awesome check square

--- assets/vue/components/buttons/EditIcon.vue
+++ assets/vue/components/buttons/EditIcon.vue
@@ -2,54 +2,51 @@
   <div>
     <button
       type="submit"
-      class="edit"
+      @click="isEdit = !isEdit"
     >
-      <font-awesome-icon :icon="['fa', 'edit']" />
-    </button>
-
-    <button
-      type="submit"
-      class="btn btn-default btn-circle btn-lg"
-    >
-      <span class="check">
-        <font-awesome-icon :icon="'check'" />
+      <!-- Affichage du bouton edit -->
+      <span v-show="isEdit">
+        <span class="edit">
+          <font-awesome-icon :icon="['fa', 'edit']" />
+        </span>
+      </span>
+      <!-- Affichage du bouton check -->
+      <span v-show="!isEdit">
+        <span class="check">
+          <font-awesome-icon :icon="['fa', 'check-square']" />
+        </span>
       </span>
     </button>
   </div>
 </template>
 
-<!-- Icone d'édition si non check : .fa-pencil-square-o -->
-
 <script>
 export default {
-  name: "EditIcon"
+  name: "EditIcon",
+  props: {
+    isEdit: Boolean
+  },
+  methods: {}
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.btn-circle.btn-lg {
-  width: 2em;
-  height: 2em;
-  padding: 10px 16px;
-  font-size: 18px;
-  line-height: 0;
-  border-radius: 1em;
-  border: 2px solid #f3eff5;
-}
-
-.check {
-  color: #f3eff5;
-  position: relative;
-  right: 13px;
-  bottom: 6px;
-  font-size: 150%;
-}
-
 .edit {
   background: transparent;
   border: none !important;
   color: #f3eff5;
   font-size: 1.8em;
+}
+.check {
+  background: transparent;
+  border: none !important;
+  color: #f3eff5;
+  font-size: 1.8em;
+}
+
+button {
+  background: transparent;
+  border: none !important;
 }
 </style>
